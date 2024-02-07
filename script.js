a2c61112f9831fe7c31862c251f78ed418aa8562
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-02-07 14:48:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-let ans=document.getElementById("output");
 
-function promise1(){
-    let arr=[1,2,3,4];
+let ans = document.getElementById("output");
 
-    return new promise((resolve,reject)=>{
-setTimeout(() => {
-    resolve(arr);
-}, 3000);
+function promise1() {
+    let arr = [1, 2, 3, 4];
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(arr);
+        }, 3000);
+    });
+}
+
+function promise2(arr) {
+    return new Promise((resolve, reject) => {
+        let oddarr = arr.filter((res) => res % 2 != 0)
+
+        setTimeout(() => {
+            resolve(oddarr)
+        }, 1000)
     })
 }
-function promise2(arr){
-    return new Promise((resolve,reject) => {
-		let oddarr=arr.filter((res) => res%2!=0)
-		
-		setTimeout(() => {
-			resolve(oddarr)
-		},1000)
-	})
+
+function promise3(arr) {
+    return new Promise((resolve, reject) => {
+        let doublearr = arr.map((val) => val * 2)
+        setTimeout(() => {
+            resolve(doublearr)
+        }, 2000)
+    })
 }
 
-function promise3(arr){
-	return new Promise((resolve,reject) => {
-		let doublearr=arr.map((val) => val*2)
-		setTimeout(() => {
-			resolve(doublearr)
-		},2000)
-	})
-}
-
-promise1().then((arr)=>{
-     return promise2(arr);
-}).then((arr)=>{
-     ans.innerHTML=arr;
-      return promise3(arr);
-}).then((arr)=>{
-    ans.innerHTML=arr;
-})
-
-
-    
+promise1().then((arr) => {
+    return promise2(arr);
+}).then((arr) => {
+    ans.innerHTML = arr;
+    return promise3(arr);
+}).then((arr) => {
+    ans.innerHTML = arr;
+});
